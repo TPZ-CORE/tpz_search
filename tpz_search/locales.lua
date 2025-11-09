e885671d193d99ea63b2bf9e5163fc7cfe9f5867
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-11-09 17:09:01 +0200

    Update locales.lua

--- tpz_search/locales.lua
+++ tpz_search/locales.lua
@@ -6,5 +6,7 @@ Locales = {
     ['PLAYER_TARGET_UNDER_PROTECTION']  = { text = "The specified player is new to the server, you can't search this player at the moment.", duration = 7 },
     ['PLAYER_TARGET_BLACKLISTED_JOB']   = { text = "The specified player cannot be searched.", duration = 4 },
     ['CANNOT_SEARCH_PLAYER_IS_BUSY']    = { text = "You cannot search this person while being already searched.", duration = 5 },  
-
+    
+    ['DEVTOOLS_INJECTION_DETECTED_TITLE_LOG'] = "DevTools / Injection Cheat Found!",
+    ['DEVTOOLS_INJECTION_DETECTED']           = "You have been kicked due to cheating by using DevTools or Injection cheat.",
 }
