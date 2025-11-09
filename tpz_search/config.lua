13e3394034eb51a47ebd1e32f5ed8214eebc981c
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-11-09 17:09:47 +0200

    Update config.lua

--- tpz_search/config.lua
+++ tpz_search/config.lua
@@ -88,4 +88,16 @@ function SendNotification(source, message, notifyType, duration)
   
 end
 
+-----------------------------------------------------------
+--[[ Webhooking (Only DevTools - Injection Cheat Logs) ]]--
+-----------------------------------------------------------
 
+Config.Webhooks = {
+    
+    ['DEVTOOLS_INJECTION_CHEAT'] = { -- Warnings and Logs about players who used or atleast tried to use devtools injection.
+        Enabled = false, 
+        Url = "", 
+        Color = 10038562,
+    },
+
+}
