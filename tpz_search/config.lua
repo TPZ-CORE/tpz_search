cdb061809982e014510c2517fa9c937e6781d92c
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-01-04 13:57:22 +0200

    Update config.lua

--- tpz_search/config.lua
+++ tpz_search/config.lua
@@ -92,14 +92,14 @@ end
 -----------------------------------------------------------
 --[[ Webhooking (Only DevTools - Injection Cheat Logs) ]]--
 -----------------------------------------------------------
-
+-- (!) Checkout tpz_core/server/discord/webhooks.lua to modify the webhook urls.
 Config.Webhooks = {
     
     ['DEVTOOLS_INJECTION_CHEAT'] = { -- Warnings and Logs about players who used or atleast tried to use devtools injection.
         Enabled = false, 
-        Url = "", 
         Color = 10038562,
     },
 
 }
+
 
